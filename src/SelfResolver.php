ef2231a1e75ca80dfc1dfb0e1bc1901db5704fca
Omran Jamal <o.jamal97@gmail.com>
2016-07-07 16:23:31 +0600

    Blocked Self refferencing

--- src/SelfResolver.php
+++ src/SelfResolver.php
@@ -5,6 +5,10 @@ class SelfResolver extends MagicResolver
 {
     public function get($property)
     {
+        if ($property === $this->property) {
+            throw new \Exception("Can't have a self refference.");
+        }
+
         return $this->seeder->resolveValue(
             $this->i,
             $property,
